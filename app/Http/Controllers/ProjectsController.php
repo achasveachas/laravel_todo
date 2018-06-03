7e5505af108a1222ff559b1238d54d8003cbaecb
Yechiel Kalmenson <contact@yechiel.me>
2018-06-03 13:38:41 -0400

    Add index and create controllers and boilerplate views

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('projects.index');
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
 }
