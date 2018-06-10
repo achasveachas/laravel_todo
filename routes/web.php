5675db1efccc80a86dcbe92fa9c1450f8ea14318
Yechiel Kalmenson <contact@yechiel.me>
2018-06-10 19:54:47 -0400

    fix the routing

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ Route::model('tasks', 'Task');
 Route::model('projects', 'Project');
 
 Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
+
 Route::bind('projects', function($value, $route) {
 	return App\Project::whereSlug($value)->first();
 });
-
-Route::resource('projects.tasks', 'TasksController');
 Route::bind('tasks', function($value, $route) {
 	return App\Task::whereSlug($value)->first();
 });
