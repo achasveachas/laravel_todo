d16adc26cade24a8a9165d5961324c51441ffbf1
Yechiel Kalmenson <contact@yechiel.me>
2018-06-10 20:17:19 -0400

    astablish a has-many/belongs-to relationship between project and tasks

--- app/Project.php
+++ app/Project.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    // Use slugs as the key in the routing
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Task');
     }
 }
--- app/Task.php
+++ app/Task.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    // Use slugs as the key in the routing
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function project()
+    {
+        return $this->belongsTo('App\Project');
     }
 }
