8d5b50d5040250edf7b3a4946c53d01728e885f5
Yechiel Kalmenson <contact@yechiel.me>
2018-06-10 20:08:54 -0400

    fix the routing to use slugs

--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,3 @@ Route::model('projects', 'Project');
 
 Route::resource('projects', 'ProjectsController');
 Route::resource('projects.tasks', 'TasksController');
-
-Route::bind('projects', function($value, $route) {
-	return App\Project::whereSlug($value)->first();
-});
-Route::bind('tasks', function($value, $route) {
-	return App\Task::whereSlug($value)->first();
-});
